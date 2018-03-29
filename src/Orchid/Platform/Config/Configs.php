61e87b3490ddc4c346e6fb03786b7aa51b69dc67
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2018-03-29 15:43:50 +0200

    Latest update

--- src/Orchid/Platform/Config/Configs.php
+++ src/Orchid/Platform/Config/Configs.php
@@ -5,4 +5,6 @@ use Orchid\Core\Config\ConfigurationInterface;
 return [
     ConfigurationInterface::KEY_APP_ROOT => '.',
     ConfigurationInterface::KEY_CONFIG_ROOT => 'config',
-    ConfigurationInterface::KEY_CONTENT_ROOT => 'content'];
+    ConfigurationInterface::KEY_CONTENT_ROOT => 'content',
+    ConfigurationInterface::KEY_CACHE_ROOT => 'cache',
+    ConfigurationInterface::KEY_THEMES_ROOT => 'themes'];
